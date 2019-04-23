c36e66301b2a2bff8cd1204ca08480423b2444cf
DESKTOP-HPIEAC5\Rambo <kuba11.labaz@gmail.com>
2019-04-23 19:14:53 +0200

    Added Planets and Orbit script

---
+++ Solar_System_Sym/Assets/Scripts/Orbit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject Target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitAround();
+    }
+
+    private void OrbitAround()
+    {
+        transform.RotateAround (Target.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+    
+}
