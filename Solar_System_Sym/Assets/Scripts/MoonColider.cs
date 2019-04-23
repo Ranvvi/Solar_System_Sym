d19e21059af32c803b1c8b3aa52ebbcf4212d08e
DESKTOP-HPIEAC5\Rambo <kuba11.labaz@gmail.com>
2019-04-23 19:55:54 +0200

    Added assets and effects

---
+++ Solar_System_Sym/Assets/Scripts/MoonColider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonColider : MonoBehaviour
+{
+    public GameObject objToDestroy;
+    public GameObject effect;
+
+   
+   void OnTriggerDestroy(Collider other)
+    {
+        if(other.gameObject.tag == "Moon")
+        {
+            Instantiate(effect, objToDestroy.transform.position, objToDestroy.transform.rotation);
+            Destroy(objToDestroy);
+        }
+    }
+}
--- Solar_System_Sym/Assets/Scripts/Orbit.cs
+++ Solar_System_Sym/Assets/Scripts/Orbit.cs
@@ -7,10 +7,6 @@ public class Orbit : MonoBehaviour
     public GameObject Target;
     public float speed;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
